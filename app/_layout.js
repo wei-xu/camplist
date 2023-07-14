e1b1b34e8de1ceab26d6539a89c7adda80ac2ce7
wxu <ginues109@gmail.com>
2023-07-13 22:25:15 -0700

    major commit

--- app/_layout.js
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-import { Stack } from "expo-router";
-import React from "react";
-
-const _layout = () => {
-  return <Stack />;
-};
-
-export default _layout;
